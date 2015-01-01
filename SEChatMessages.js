9fe465b2773f41cf2972ef834a89de4892f9b7a1
Anej <easwee@gmx.com>
2015-01-01 20:22:20 +0100

    Added luminance check
    Added color luminance check to invert text color on darker hues.

--- SEChatMessages.js
+++ SEChatMessages.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         SE Rainbow messages!
-// @namespace    http://easwee.net/
+// @namespace    http://www.easwee.net/
 // @version      0.1
 // @description  SE chat message background hue changer. Script is meant to be run in Tampermonkey or similar browser addon.
 // @author       easwee
@@ -11,6 +11,7 @@
 var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
 var chat = document.getElementById('chat');
 var frequency = .3;
+var red, green, blue, luma, color;
 
 var observer = new MutationObserver(function(mutations) {
   mutations.forEach(function(mutation) {
@@ -22,7 +23,12 @@ var observer = new MutationObserver(function(mutations) {
         green = Math.sin(frequency * i + 2) * 127 + 128;
         blue  = Math.sin(frequency * i + 4) * 127 + 128;
 
-        messages[i].style.backgroundColor = rgba2color(red, green, blue, 0.5);
+        luminance = 0.2126 * red + 0.7152 * green + 0.0722 * blue;
+
+        var color = luminance < 120 ? 'white' : 'black';
+
+        messages[i].style.backgroundColor = rgba2color(red, green, blue, 0.8);
+        messages[i].style.color = color;
       }
     }
   });
